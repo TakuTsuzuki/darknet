e59e2bdbb49274eec2a2279404cde829abade709
Taku Tsuzuki <tsuzuki@systems-biology.jp>
2021-02-08 14:54:14 +0900

    add include guard to distribution.h

--- src/distribution.h
+++ src/distribution.h
@@ -1,9 +1,13 @@
+#ifndef DISTRIBUTION_H
+#define DISTRIBUTION_H
 #include "darknet.h" 
 
 void init_scale_mixture_gaussian(ScaleMixtureGaussian *smg, float mu1, float sigma1, float mu2, float sigma2, float alpha); 
 float compute_log_prob_gaussian(Gaussian *dist, float x); 
 float compute_log_prob_scale_mixture_gaussian(ScaleMixtureGaussian *dist, float x); 
 
-//おかしいので直す
+//おかしいかもしれない
 float _compute_log_prob_gaussian(float mu, float sigma, float x) ;
 float _compute_prob_gaussian(float mu, float sigma, float x) ; 
+
+#endif
